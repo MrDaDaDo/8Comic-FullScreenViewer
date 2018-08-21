dcf166c9e4018a670f429825b70c107c4709a7d2
MrDaDaDo <42548841+MrDaDaDo@users.noreply.github.com>
2018-08-22 02:45:11 +0900

    bug fix

--- 8Comic-FullScreenViewer.user.js
+++ 8Comic-FullScreenViewer.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         8Comic-FullScreenViewer
-// @version      1.1
+// @version      1.2
 // @author       MrDaDaDo
-// @include      /^https:\/\/v\.comicbus\.com\/online\/(comic|manga)\-(\d)+\.html\?ch=(\d)+(\-(\d)+)?/
+// @include      /^https:\/\/v\.comicbus\.com\/online\/(comic|manga)(\-|_)(\d)+\.html\?ch=(\d)+(\-(\d)+)?/
 // @require      https://code.jquery.com/jquery-3.3.1.min.js
 // ==/UserScript==
 
