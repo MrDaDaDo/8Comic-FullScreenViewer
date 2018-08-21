4fdeb5b65fd2be2dc4587569b9706dc43ccae301
MrDaDaDo <42548841+MrDaDaDo@users.noreply.github.com>
2018-08-22 02:28:26 +0900

    first commit

---
+++ 8Comic-FullScreenViewer.user.js
@@ -0,0 +1,49 @@
+// ==UserScript==
+// @name         8Comic-FullScreenViewer
+// @version      1.0
+// @author       MrDaDaDo
+// @include      /^https:\/\/v\.comicbus\.com\/online\/comic\-(\d)+\.html\?ch=(\d)+(\-(\d)+)?/
+// @require      https://code.jquery.com/jquery-3.3.1.min.js
+// ==/UserScript==
+
+(function() {
+    var $ = window.jQuery
+    var $img = $('#TheImg');
+    var $td = $img.parent();
+    var isFullScreen = false;
+    $img.css('object-fit','contain');
+    $img.on('click',null,function() { jn(); });
+    $img.on('contextmenu',null,function() { unFullScreen(); });
+    var fullScreen = function() {
+        $td.css('position', 'fixed');
+        $td.css('top', '0px');
+        $td.css('left', '0px');
+        $td.css('width', '100%');
+        $td.css('height', '100%');
+        $td.css('background-color', '#666666');
+        $img.css('width','100%');
+        $img.css('height','100%');
+        $img.css('width','100%');
+        isFullScreen = true;
+    };
+    var unFullScreen = function() {
+        $td.css('position', '');
+        $td.css('top', '');
+        $td.css('left', '');
+        $td.css('width', '');
+        $td.css('height', '');
+        $td.css('background-color', '');
+        $img.css('width','');
+        $img.css('height','');
+        $img.css('width','');
+        isFullScreen = false;
+    };
+    var changeFullScreen = function() {
+        if(isFullScreen) {
+            unFullScreen();
+        } else {
+            fullScreen();
+        }
+    };
+    fullScreen();
+})();
---
+++ README.md
@@ -0,0 +1,12 @@
+# 8Comic-FullScreenViewer
+- FullScreen automatic
+
+# How to Use
+Step1. You need Tampermonkey.
+
+- Chrome: [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=zh-TW)
+  
+- FireFox: [Tampermonkey](https://addons.mozilla.org/zh-TW/firefox/addon/tampermonkey/)
+  
+Step2. Install script
+- Click [8Comic-FullScreenViewer.user.js](https://github.com/MrDaDaDo/8Comic-FullScreenViewer/raw/master/8Comic-FullScreenViewer.user.js) to install script.
